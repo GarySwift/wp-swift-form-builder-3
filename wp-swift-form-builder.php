87ac1ddd96fc88eaf90ca830d6cc352b4dce0ae4
Gary Swift <garyswiftmail@gmail.com>
2019-02-04 12:18:35 +0000

    Removed filter for acf title

--- wp-swift-form-builder.php
+++ wp-swift-form-builder.php
@@ -214,7 +214,7 @@ run_wp_swift_form_builder();
 /*   Friendly Block Titles                                  */
 /***********************************************************/
 
-function my_layout_title($title, $field, $layout, $i) {
+function form_builder_acf_layout_title($title, $field, $layout, $i) {
 	if($value = get_sub_field('name')) {
 		return $value . ' <sup>(' . $title . ')</sup>';
 	} else {
@@ -228,4 +228,4 @@ function my_layout_title($title, $field, $layout, $i) {
 	}
 	return $title;
 }
-add_filter('acf/fields/flexible_content/layout_title', 'my_layout_title', 10, 4);
+// add_filter('acf/fields/flexible_content/layout_title', 'form_builder_acf_layout_title', 10, 4);
